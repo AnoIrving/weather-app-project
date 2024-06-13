ce44aeef98e23e6f8293d92bd88128e6f357eeac
AnoIrving <oairving@gmail.com>
2024-06-13 11:50:26 -0500

    start JS search submit form

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function handleSearchSubmit(event) {
+  event.preventDefult();
+  let searchInout = document.querySelector("#search-form-input");
+}
+
+let searchFormElement = document.querySelector("search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
