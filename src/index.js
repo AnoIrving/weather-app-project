70765747d34ae02b89350a0905bd7f914fa31b40
AnoIrving <oairving@gmail.com>
2024-06-14 17:29:01 -0500

    added current weather and current date

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
 function updateCurrentWeather(response) {
-  let currentTemp = Math.round(response.data.temperature.current);
   let currentTempElement = document.querySelector("#current-temp");
   let cityElement = document.querySelector("#city");
+  let conditionsElement = document.querySelector("#conditions");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let heatIndexElement = document.querySelector("#heat-index");
+  let dateElement = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
 
+  dateElement.innerHTML = currentDate(date);
+  heatIndexElement.innerHTML = `${Math.round(
+    response.data.temperature.feels_like
+  )}°F`;
+  windElement.innerHTML = `${response.data.wind.speed} mph`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  conditionsElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
-  currentTempElement.innerHTML = currentTemp;
+  currentTempElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function currentDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} <strong>${hours}:${minutes}</strong>`;
 }
 
 function searchCity(city) {
